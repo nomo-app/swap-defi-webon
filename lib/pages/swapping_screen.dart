54adfe0265a8b59c99c8067d3d68a05fdc4a9e46
dev2-nomo <dev2@nomo.app>
2024-01-25 17:06:10 +0100

    remove appbar from webon

--- lib/pages/swapping_screen.dart
+++ lib/pages/swapping_screen.dart
@@ -1,36 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nomo_router/router/nomo_navigator.dart';
 import 'package:nomo_ui_kit/components/app/routebody/nomo_route_body.dart';
 import 'package:nomo_ui_kit/components/buttons/secondary/nomo_secondary_button.dart';
 import 'package:nomo_ui_kit/icons/nomo_icons.dart';
 import 'package:nomo_ui_kit/theme/nomo_theme.dart';
+import 'package:nomo_ui_kit/theme/theme_provider.dart';
 import 'package:swapping_webon/routes.dart';
+import 'package:swapping_webon/theme/theme.dart';
 import 'package:swapping_webon/widgets/swap_card.dart';
 
-class SwappingScreen extends StatelessWidget {
+class SwappingScreen extends ConsumerWidget {
   const SwappingScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeProvider = ThemeProvider.of(context);
     return NomoRouteBody(
       builder: (context, route) => Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Align(
-            alignment: Alignment.centerRight,
-            child: SecondaryNomoButton(
-              onPressed: () {
-                NomoNavigator.of(context).push(HistoryScreenRoute());
-              },
-              shape: BoxShape.circle,
-              padding: const EdgeInsets.all(8),
-              iconSize: 22,
-              textStyle: context.theme.typography.b2.copyWith(
-                fontWeight: FontWeight.bold,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              if (themeProvider.colorTheme == ColorMode.DARK.theme)
+                IconButton(
+                  onPressed: () {
+                    ThemeProvider.of(context).changeColorTheme(
+                      ColorMode.LIGHT.theme,
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.light_mode,
+                    color: Colors.white,
+                  ),
+                )
+              else
+                IconButton(
+                  onPressed: () {
+                    ThemeProvider.of(context).changeColorTheme(
+                      ColorMode.DARK.theme,
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.dark_mode,
+                  ),
+                ),
+              const SizedBox(width: 16),
+              SecondaryNomoButton(
+                onPressed: () {
+                  NomoNavigator.of(context).push(HistoryScreenRoute());
+                },
+                shape: BoxShape.circle,
+                padding: const EdgeInsets.all(8),
+                iconSize: 22,
+                textStyle: context.theme.typography.b2.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+                // ignore: deprecated_member_use
+                icon: NomoIcons.history,
               ),
-              // ignore: deprecated_member_use
-              icon: NomoIcons.history,
-            ),
+            ],
           ),
           const SizedBox(height: 16),
           const SwapCard(),
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,60 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:nomo_router/router/entities/route.dart';
-import 'package:nomo_ui_kit/components/app/app_bar/nomo_app_bar.dart';
 import 'package:nomo_ui_kit/components/app/scaffold/nomo_scaffold.dart';
 import 'package:nomo_ui_kit/nomo_ui_kit_base.dart';
-import 'package:nomo_ui_kit/theme/nomo_theme.dart';
-import 'package:nomo_ui_kit/theme/theme_provider.dart';
 import 'package:route_gen/anotations.dart';
 import 'package:swapping_webon/pages/history_screen.dart';
 import 'package:swapping_webon/pages/swapping_screen.dart';
-import 'package:swapping_webon/theme/theme.dart';
 
 part 'routes.g.dart';
 
 Widget wrapper(nav) {
   return Builder(
     builder: (context) {
-      final themeProvider = ThemeProvider.of(context);
       return NomoScaffold(
-        appBar: PreferredSize(
-          preferredSize:
-              Size.fromHeight(context.componentSizes.appBarTheme.height),
-          child: NomoAppBar(
-            leading: Text(
-              "Swap",
-              style: context.typography.h3,
-            ),
-            trailling: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (themeProvider.colorTheme == ColorMode.DARK.theme)
-                  IconButton(
-                    onPressed: () {
-                      ThemeProvider.of(context).changeColorTheme(
-                        ColorMode.LIGHT.theme,
-                      );
-                    },
-                    icon: const Icon(
-                      Icons.light_mode,
-                      color: Colors.white,
-                    ),
-                  )
-                else
-                  IconButton(
-                    onPressed: () {
-                      ThemeProvider.of(context).changeColorTheme(
-                        ColorMode.DARK.theme,
-                      );
-                    },
-                    icon: const Icon(
-                      Icons.dark_mode,
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        ),
         child: nav,
       );
     },
