b2829f5e31a520d38eec164b4ae6657cf2b76084
nomo-fe <dev0@nomo.app>
2024-02-22 13:28:53 +0100

    update the cli to comply with new requirements

--- nomo_cli.config.cjs
+++ nomo_cli.config.cjs
@@ -5,6 +5,7 @@ const nomoCliConfig = {
         sshHost: process.env.SSH_TARGET,
         sshBaseDir: "/var/www/production_webons/swapdefi/",
         publicBaseUrl: "https://w.nomo.app/swapdefi",
+        hybrid: true,
       },
     },
   },
